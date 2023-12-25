f7910bacb29abe41fb16b433d05f55b5a3dcf706
Zamir Guliyev <tu54h1k30@code.edu.az>
2023-12-25 13:57:35 +0400

    updated

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,7 +1,26 @@
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const Post = () => {
   return (
-    <div>Post</div>
+    <div className="App">
+                <CKEditor
+                    editor={ ClassicEditor }
+                    data=""
+                    onReady={ editor => {
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event ) => {
+                        console.log( event );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
+            </div>
   )
 }
 
