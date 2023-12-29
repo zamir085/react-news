db7a2a87c152f337fcfca750143999115dd965bb
zamirguliyev <zamirquliyev7@gmail.com>
2023-12-29 22:59:45 +0400

    updated

--- src/components/PublisherPage.tsx
+++ src/components/PublisherPage.tsx
@@ -1,12 +1,34 @@
-import { useEffect, useState } from 'react';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
-import { Modal, Input, Button, notification, Image, Row, Col, Card } from 'antd';
-import { updatePublisher, getPublisher, getAllPublishers } from '../services/publishersApi';
-import { getNewsByPublisher } from '../services/newsApi';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import {
+  Modal,
+  Input,
+  Button,
+  notification,
+  Image,
+  Row,
+  Col,
+  Card,
+} from "antd";
+import {
+  updatePublisher,
+  getPublisher,
+  getAllPublishers,
+} from "../services/publishersApi";
+import {
+  getNewsByPublisher,
+  deleteNews,
+  updateNews,
+} from "../services/newsApi";
+import { Link } from "react-router-dom";
 const { Meta } = Card;
-import { ClockCircleOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import {
+  ClockCircleOutlined,
+  EditOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
+import EditNewsModal from "./EditNewsModal";
 
 interface NewsType {
   _id: string;
@@ -17,28 +39,32 @@ interface NewsType {
   title: string;
 }
 
-
 const PublisherPage = () => {
-  const [userId, setUserId] = useState('');
+  const [userId, setUserId] = useState("");
   const [userData, setUserData] = useState({
-    _id: '',
-    username: '',
-    name: '',
-    profileImg: '',
-    email: '',
-    password: '',
-    description: ''
+    _id: "",
+    username: "",
+    name: "",
+    profileImg: "",
+    email: "",
+    password: "",
+    description: "",
   });
 
   const [visiblePassword, setVisiblePassword] = useState(false);
   const [visibleInfo, setVisibleInfo] = useState(false);
   const [existingUsers, setExistingUsers] = useState([]);
   const [publisherNews, setPublisherNews] = useState<NewsType[]>([]);
+  const [editModalVisible, setEditModalVisible] = useState<boolean>(false);
+  const [selectedNews, setSelectedNews] = useState<NewsType | null>(null);
 
-  const userLocale = JSON.parse(localStorage.getItem('publisher') || '{}');
+
+  const userLocale = JSON.parse(localStorage.getItem("publisher") || "{}");
 
   useEffect(() => {
-    const userFromLocalStorage = JSON.parse(localStorage.getItem('publisher') || '{}');
+    const userFromLocalStorage = JSON.parse(
+      localStorage.getItem("publisher") || "{}"
+    );
     setUserId(userFromLocalStorage.id);
     getPublisher(userFromLocalStorage.id).then((data) => {
       setUserData(data);
@@ -47,32 +73,29 @@ const PublisherPage = () => {
     getAllPublishers().then((users) => {
       setExistingUsers(users);
     });
-
-
-
   }, []);
 
   useEffect(() => {
     getNewsByPublisher(userLocale.id).then((news) => {
       setPublisherNews(news);
     });
-  }, [])
+  }, []);
 
-  console.log(publisherNews)
+  console.log(publisherNews);
 
   const handleUpdatePassword = (values) => {
     if (values.currentPassword !== userData.password) {
       notification.error({
-        message: 'Error',
-        description: 'Current password is incorrect!',
+        message: "Error",
+        description: "Current password is incorrect!",
       });
       return;
     }
 
     if (values.newPassword !== values.confirmPassword) {
       notification.error({
-        message: 'Error',
-        description: 'New password and confirm password do not match!',
+        message: "Error",
+        description: "New password and confirm password do not match!",
       });
       return;
     }
@@ -83,28 +106,37 @@ const PublisherPage = () => {
 
   const handleUpdateInfo = async (values) => {
     try {
-      const isUsernameExists = existingUsers.some(user => user.username === values.username);
+      const isUsernameExists = existingUsers.some(
+        (user) => user.username === values.username
+      );
 
       if (isUsernameExists && userData.username !== values.username) {
         notification.warning({
-          message: 'Warning',
-          description: 'This username already exists!',
+          message: "Warning",
+          description: "This username already exists!",
         });
       } else {
         if (userData.username !== values.username) {
-          const updatedUsers = existingUsers.filter(user => user.username !== userData.username);
+          const updatedUsers = existingUsers.filter(
+            (user) => user.username !== userData.username
+          );
           setExistingUsers(updatedUsers);
 
-          const userFromLocalStorage = JSON.parse(localStorage.getItem('publisher') || '{}');
+          const userFromLocalStorage = JSON.parse(
+            localStorage.getItem("publisher") || "{}"
+          );
           userFromLocalStorage.username = values.username;
-          localStorage.setItem('publisher', JSON.stringify(userFromLocalStorage));
+          localStorage.setItem(
+            "publisher",
+            JSON.stringify(userFromLocalStorage)
+          );
         }
 
         await updatePublisher(userId, values);
         setVisibleInfo(false);
 
         if (userData.username !== values.username) {
-          setUserData(prevUserData => ({
+          setUserData((prevUserData) => ({
             ...prevUserData,
             username: values.username,
             name: values.name,
@@ -116,26 +148,29 @@ const PublisherPage = () => {
     } catch (error) {
       console.error(error);
       notification.error({
-        message: 'Error',
-        description: 'Failed to update user information!',
+        message: "Error",
+        description: "Failed to update user information!",
       });
     }
   };
 
-
-
-
   const validationSchemaPassword = Yup.object().shape({
-    currentPassword: Yup.string().required('Current password is required'),
-    newPassword: Yup.string().min(8, 'Password must be at least 8 characters').required('New password is required'),
-    confirmPassword: Yup.string().oneOf([Yup.ref('newPassword'), null], 'Passwords must match').required('Confirm password is required'),
+    currentPassword: Yup.string().required("Current password is required"),
+    newPassword: Yup.string()
+      .min(8, "Password must be at least 8 characters")
+      .required("New password is required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("newPassword")], "Passwords must match")
+      .required("Confirm password is required"),
   });
 
   const validationSchemaInfo = Yup.object().shape({
-    username: Yup.string().required('Username is required'),
-    name: Yup.string().required('Full name is required'),
-    profileImg: Yup.string().url('Invalid URL format').required('Profile image is required'),
-    description: Yup.string().required('Profile image is required'),
+    username: Yup.string().required("Username is required"),
+    name: Yup.string().required("Full name is required"),
+    profileImg: Yup.string()
+      .url("Invalid URL format")
+      .required("Profile image is required"),
+    description: Yup.string().required("Profile image is required"),
   });
 
   const handleEditPassword = () => {
@@ -146,10 +181,39 @@ const PublisherPage = () => {
     setVisibleInfo(true);
   };
 
+  const handleDeleteNews = async (newsId: string) => {
+    try {
+      await deleteNews(newsId);
+      setPublisherNews((prevNews) =>
+        prevNews.filter((news) => news._id !== newsId)
+      );
+      notification.success({
+        message: "Success",
+        description: "News deleted successfully!",
+      });
+    } catch (error) {
+      console.error(error);
+      notification.error({
+        message: "Error",
+        description: "Failed to delete news!",
+      });
+    }
+  };
+
+  const handleEditModalOpen = (news: NewsType) => {
+    setSelectedNews(news);
+    setEditModalVisible(true);
+  };
+
+  const handleEditModalClose = () => {
+    setEditModalVisible(false);
+  };
+
+
   return (
     <div>
       <h1>Publisher Detail</h1>
-      <Image width={64} src={userData.profileImg} ></Image>
+      <Image width={64} src={userData.profileImg}></Image>
       <p>Username: {userData.username}</p>
       <p>Full Name: {userData.name}</p>
       <p>Desc: {userData.description}</p>
@@ -164,9 +228,9 @@ const PublisherPage = () => {
       >
         <Formik
           initialValues={{
-            currentPassword: '',
-            newPassword: '',
-            confirmPassword: '',
+            currentPassword: "",
+            newPassword: "",
+            confirmPassword: "",
           }}
           validationSchema={validationSchemaPassword}
           onSubmit={handleUpdatePassword}
@@ -226,9 +290,7 @@ const PublisherPage = () => {
               <div>
                 <label>Name:</label>
                 <Field name="name" as={Input} />
-                {errors.name && touched.name && (
-                  <div>{errors.name}</div>
-                )}
+                {errors.name && touched.name && <div>{errors.name}</div>}
               </div>
               <div>
                 <label>Desciption:</label>
@@ -251,27 +313,65 @@ const PublisherPage = () => {
           )}
         </Formik>
       </Modal>
-      <Row gutter={[16, 16]} style={{ padding: '40px 25px' }}>
+      <Row gutter={[16, 16]} style={{ padding: "40px 25px" }}>
         {publisherNews.map((news) => (
           <Col xs={24} sm={12} md={8} key={news._id}>
-            <Card  hoverable cover={<img alt={news.title} src={news.thumbnailImg} />}>
+            <Card
+              hoverable
+              cover={<img alt={news.title} src={news.thumbnailImg} />}
+            >
               <Meta title={news.title} />
-              <div dangerouslySetInnerHTML={{ __html: news?.newsBody || '' }} />
-              <div style={{ marginTop: '12px' }}>
+              <div dangerouslySetInnerHTML={{ __html: news?.newsBody || "" }} />
+              <div style={{ marginTop: "12px" }}>
                 <ClockCircleOutlined />
-                <span style={{ marginLeft: '8px' }}>{news.createdAt}</span>
+                <span style={{ marginLeft: "8px" }}>{news.createdAt}</span>
               </div>
-              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
                 <Link to={`/news/${news._id}`}>Read More</Link>
-                <div style={{display:'flex',gap:'20px'}}>
-                  <EditOutlined style={{fontSize:'20px',color:'green'}} />
-                  <DeleteOutlined style={{fontSize:'20px', color:'red'}} />
+                <div style={{ display: "flex", gap: "20px" }}>
+                  <EditOutlined
+                    style={{ fontSize: "20px", color: "green" }}
+                    onClick={() => handleEditModalOpen(news)}
+                  />
+                  <DeleteOutlined
+                    style={{ fontSize: "20px", color: "red" }}
+                    onClick={() => handleDeleteNews(news._id)}
+                  />
                 </div>
               </div>
             </Card>
           </Col>
         ))}
       </Row>
+      <Modal
+        title="Edit News"
+        visible={editModalVisible}
+        onCancel={handleEditModalClose}
+        footer={null}
+      >
+        {selectedNews && (
+          <EditNewsModal
+            initialValues={{
+              title: selectedNews.title,
+              linkURL: selectedNews.linkURL,
+              thumbnailImg: selectedNews.thumbnailImg,
+              newsBody: selectedNews.newsBody,
+            }}
+            onSubmit={(values) => {
+              updateNews(selectedNews._id, values);
+              handleEditModalClose();
+            }}
+            onCancel={handleEditModalClose}
+          />
+        )}
+      </Modal>
+
     </div>
   );
 };
